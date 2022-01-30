74ffae0dab99173a86bf00ccbffb2a38cff88e16
Sam Apoola <71335145+b0wd3n@users.noreply.github.com>
2022-01-30 01:13:21 +0000

    working ASYNCIO get_player_hist

--- src/get_data.py
+++ src/get_data.py
@@ -6,6 +6,7 @@ import asyncio
 import json
 import aiohttp
 from understat import Understat
+import httpx
 
 
 #%% Get data function
@@ -22,39 +23,48 @@ def get_data(url="https://fantasy.premierleague.com/api/bootstrap-static/"):
 
 #%% Get player hist
 
-def get_player_hist(i, url='https://fantasy.premierleague.com/api/element-summary/'):
+async def get_player_hist(player_ids, url='https://fantasy.premierleague.com/api/element-summary/', save_to_file=True):
     ''''
     Retrive fixtures, season so far data, and historical data 
     '''
-    response = requests.get(url+str(i)+'/')
-    if response.status_code != 200:
-        raise Exception('Respone was code ' + str(response.status_code))
-    return json.loads(response.text)
+
+    async with httpx.AsyncClient() as client:
+        tasks = (client.get(url + str(player_id) + '/') for player_id in player_ids)
+        responses = await asyncio.gather(*tasks)
+
+    # dict of dicts with player ID as key
+    data = {int((str(response.url)).split('/')[-2]):response.json() for response in responses}
+
+    if save_to_file:
+        with open('.data/history.json', 'w') as outf:
+            json.dump(data, outf)
+
+    return data
 
 
+#%% understat data
 
-#%% Unistat data
+def get_understat(induvidual_stats=False):
+    '''
+    Understat data getter
+    '''
 
-async def unistat(ind_stats):
-    async with aiohttp.ClientSession() as session:
-        understat = Understat(session)
-        players = await understat.get_league_players(
-            'epl', 2021
-        )
-        #name = [player['player_name'] for player in players]
-        #print(name[:25])
-        if ind_stats:
-            for player in players:
-                with open('../.data/player_data/'+player['player_name']+'.json', 'w') as player_data:
-                    json.dump(player, player_data)
+    async def understat_getter(induvidual_stats):
+        async with aiohttp.ClientSession() as session:
+            understat = Understat(session)
+            players = await understat.get_league_players('epl', 2021)
             
-        with open('../.data/raw_unistats.json', 'w') as outf:
-            json.dump(players, outf)
+            if induvidual_stats:
+                for player in players:
+                    with open('../.data/player_data/'+player['player_name']+'.json', 'w') as player_data:
+                        json.dump(player, player_data)
+                
+            with open('../.data/raw_understats.json', 'w') as outf:
+                json.dump(players, outf)
 
-
-def get_unistat(ind_stats=False):
+    # Running the getters
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(unistat(ind_stats))
+    loop.run_until_complete(understat_getter(induvidual_stats))
 
 
 
@@ -63,8 +73,8 @@ def get_unistat(ind_stats=False):
 def main():
     data = get_data()
     print(type(data))
-    #print(x)
-    get_unistat(ind_stats=True)
+    # print(x)
+    get_understat(induvidual_stats=True)
 
 #%% If name main
 if __name__ == '__main__':
